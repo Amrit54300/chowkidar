4894142402a7d2201abccadeca0b3d6b74fc3b7a
Amrit54300 <35948782+Amrit54300@users.noreply.github.com>
2023-03-29 03:04:01 +0530

    Upload Software

--- script.js
+++ script.js
@@ -1,12 +1,2 @@
-const { exec } = require('child_process');
 
-function commandnew() {
-    exec('mkdir amrit', (err, stdout, stderr) => {
-        if (err) {
-            console.error(err);
-            return;
-        }
-        console.log(stdout);
-    });
-}
 
